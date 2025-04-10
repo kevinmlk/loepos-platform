e6b948383c07608b500271ca1665f79649597d0d
kevinmlk <r0900654@student.thomasmore.be>
2025-04-11 01:56:37 +0200

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Loepos</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+
+        <!-- Styles / Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="font-inter ">
+        <h1 class="text-4xl text-dark-blue">Hello Loepos!</h1>
+        <x-phosphor-alarm class="w-6 h-6 text-gray-500"/>
+    </body>
+</html>
