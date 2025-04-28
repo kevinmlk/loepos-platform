000efa16accf9f69166e508eb9895834909405a4
kevinmlk <r0900654@student.thomasmore.be>
2025-04-28 12:18:22 +0200

    Feat: Added settings item amd some styling on the navbar

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -16,47 +16,53 @@
     </head>
     <body class="font-inter text-black bg-white flex max-h-screen">
         {{-- Navigation --}}
-        <nav class="max-w-xs p-4 border-r-2 border-light-gray">
-            <ul>
-                {{-- Home nav link --}}
-                <x-nav-link-container>
-                    <x-nav-link href="/" :active="request()->is('/')">
-                        <x-phosphor-house-bold class="w-6 h-6 mr-3" />Home
-                    </x-nav-link>
-                </x-nav-link-container>
+        <nav class="max-w-xs h-screen p-4 border-r-2 border-light-gray flex flex-col justify-between">
+            <div>
+                <img src="{{ asset('images/Logo_LOEPOS_1.png') }}" alt="LoePos Logo" class="h-10 mb-8">
+                <ul>
+                    {{-- Home nav link --}}
+                    <x-nav-link-container>
+                        <x-nav-link href="/" :active="request()->is('/')">
+                            <x-phosphor-house-bold class="w-6 h-6 mr-3" />Home
+                        </x-nav-link>
+                    </x-nav-link-container>
 
-                {{-- Post processing nav link --}}
-                <x-nav-link-container>
-                    <x-nav-link href="/post-processing" :active="request()->is('post-processing')"><x-phosphor-mailbox-bold class="w-6 h-6 mr-3" />Postverwerking
-                    </x-nav-link>
-                </x-nav-link-container>
+                    {{-- Post processing nav link --}}
+                    <x-nav-link-container>
+                        <x-nav-link href="/post-processing" :active="request()->is('post-processing')"><x-phosphor-mailbox-bold class="w-6 h-6 mr-3" />Postverwerking
+                        </x-nav-link>
+                    </x-nav-link-container>
 
-                {{-- Documents nav link --}}
-                <x-nav-link-container>
-                    <x-nav-link href="/documents" :active="request()->is('documents')"><x-phosphor-folder-bold class="w-6 h-6 mr-3" />Documenten
-                    </x-nav-link>
-                </x-nav-link-container>
+                    {{-- Documents nav link --}}
+                    <x-nav-link-container>
+                        <x-nav-link href="/documents" :active="request()->is('documents')"><x-phosphor-folder-bold class="w-6 h-6 mr-3" />Documenten
+                        </x-nav-link>
+                    </x-nav-link-container>
 
-                {{-- Reports nav link --}}
-                <x-nav-link-container>
-                    <x-nav-link href="/reports" :active="request()->is('reports')"><x-phosphor-chart-bar-bold class="w-6 h-6 mr-3" />Rapporten
-                    </x-nav-link>
-                </x-nav-link-container>
+                    {{-- Reports nav link --}}
+                    <x-nav-link-container>
+                        <x-nav-link href="/reports" :active="request()->is('reports')"><x-phosphor-chart-bar-bold class="w-6 h-6 mr-3" />Rapporten
+                        </x-nav-link>
+                    </x-nav-link-container>
 
-                {{-- Support nav link --}}
-                <x-nav-link-container>
-                    <x-nav-link href="/support" :active="request()->is('support')"><x-phosphor-question-bold class="w-6 h-6 mr-3" />Ondersteuning
-                    </x-nav-link>
-                </x-nav-link-container>
-            </ul>
+                    {{-- Support nav link --}}
+                    <x-nav-link-container>
+                        <x-nav-link href="/support" :active="request()->is('support')"><x-phosphor-question-bold class="w-6 h-6 mr-3" />Ondersteuning
+                        </x-nav-link>
+                    </x-nav-link-container>
+                </ul>
+            </div>
 
-            <ul>
+            <div>
+                <x-nav-link href="/settings" :active="request()->is('settings')"><x-phosphor-gear-bold class="w-6 h-6 mr-3" />Instellingen
+                </x-nav-link>
                 {{-- Logout button --}}
                 <form method="POST" action="/logout">
-                  @csrf
-                  <x-ui.button>Afmelden</x-form.button>
+                    @csrf
+                    <x-nav-link type="button"><x-phosphor-sign-out-bold class="w-6 h-6 mr-3" />Afmelden
+                    </x-nav-link>
                 </form>
-            </ul>
+            </div>
         </nav>
 
         {{-- Main --}}
--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,1 +1,11 @@
+@props(['active' => false, 'type' => 'a'])
+
+@if ($type == 'a')
+
 <a class="{{ $active ? 'bg-light-blue' : ' hover:bg-light-blue'}} px-3 py-2 rounded-md flex content-center text-base font-semibold ml-3" aria-current="{{ request()->is('/') ? 'page' : 'false' }}" {{$attributes}}>{{ $slot }}</a>
+
+@else
+
+<button class="px-3 py-2 rounded-md flex content-center text-base font-semibold ml-3 hover:bg-light-blue cursor-pointer" {{$attributes}}>{{ $slot }}</button>
+
+@endif
