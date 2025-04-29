1a7763a6689419683022c32f0d49627b594e0cd7
kevinmlk <r0900654@student.thomasmore.be>
2025-04-29 17:22:56 +0200

    Feat: Added drag and drop functionality for file uploading - also support multiple files now

---
+++ resources/js/dropzone-config.js
@@ -0,0 +1,21 @@
+'use strict';
+
+import Dropzone from "dropzone";
+import 'dropzone/dist/dropzone.css';
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Ensure the form ID matches the one in your Blade template
+    if (document.getElementById('document-dropzone')) {
+        new Dropzone('#document-dropzone', {
+            paramName: "file", // The name that will be used to transfer the file
+            maxFilesize: 2, // MB
+            acceptedFiles: ".pdf,.png,.jpg",
+            init: function() {
+                this.on("success", function(file, response) {
+                    // Handle the response from the server
+                    console.log(response);
+                });
+            }
+        });
+    }
+});
