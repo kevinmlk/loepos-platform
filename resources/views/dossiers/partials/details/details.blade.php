cf47d12042cea54aa020a76109b08bd388c85071
kevinmlk <r0900654@student.thomasmore.be>
2025-05-06 14:22:32 +0200

    Fix: Fixed tab nav styling and displayed some dossier info

--- resources/views/dossiers/partials/details/details.blade.php
+++ resources/views/dossiers/partials/details/details.blade.php
@@ -1,4 +1,30 @@
-<p><strong>Client Name:</strong> {{ $dossier->client->first_name }} {{ $dossier->client->last_name }}</p>
-<p><strong>Status:</strong> {{ $dossier->status }}</p>
-<p><strong>Phone:</strong> {{ $dossier->client->phone }}</p>
-<p><strong>Email:</strong> {{ $dossier->client->email }}</p>
+<section class="flex flex-col">
+    <div class="grid-cols-2">
+        <article class="border-2 rounded-xl border-light-gray p-6">
+            <h2>Dossier gegevens</h2>
+            <div class="flex">
+                <div>
+                    <span>Huidige status</span>
+                    <p>{{ $dossier->status }}</p>
+                </div>
+
+                <div>
+                    <span>Type hulp</span>
+                    <p>Budgetbeheer</p>
+                </div>
+
+                <div>
+                    <span>Toegewezen medewerker</span>
+                    <p>{{ $dossier->user->first_name }} {{ $dossier->user->last_name }}</p>
+                </div>
+            </div>
+        </article>
+
+        <article class="border-2 rounded-xl border-light-gray p-6 flex gap-6 items-center">
+            <div class="bg-transparant-blue rounded-full w-20 h-20 flex justify-center items-center text-body-large font-medium text-blue">
+                {{ substr($dossier->client->first_name, 0, 1) . substr($dossier->client->last_name, 0, 1) }}
+            </div>
+            <h2>{{ $dossier->client->first_name }} {{ $dossier->client->last_name }}</h2>
+        </article>
+    </div>
+</section>
--- resources/views/dossiers/show.blade.php
+++ resources/views/dossiers/show.blade.php
@@ -8,36 +8,33 @@
     </x-header>
 
     {{-- Tab navigation --}}
-    <div class="flex border-b mb-4">
+    <div class="flex gap-4">
         <a
             href="{{ route(Route::currentRouteName(), ['dossier' => $dossier['id'], 'tab' => 'overview']) }}"
-            class="px-4 py-2 {{ request('tab', 'overview') === 'overview' ? 'border-b-2 border-blue-500 text-blue-500' : 'text-gray-500' }}"
+            class="px-4 py-2 rounded-md capitalize transition-colors duration-100 text-button font-medium {{ request('tab', 'overview') === 'overview' ? 'bg-blue text-white' : 'hover:bg-blue hover:text-white focus:outline-none' }}"
         >
             Overzicht
         </a>
 
         <a
             href="{{ route(Route::currentRouteName(), ['dossier' => $dossier['id'], 'tab' => 'documents']) }}"
-            class="px-4 py-2 {{ request('tab') === 'documents' ? 'border-b-2 border-blue-500 text-blue-500' : 'text-gray-500' }}"
+            class="px-4 py-2 rounded-md capitalize transition-colors duration-100 text-button font-medium {{ request('tab') === 'documents' ? 'bg-blue text-white' : 'hover:bg-blue hover:text-white focus:outline-none' }}"
         >
             Documenten
         </a>
 
         <a
             href="{{ route(Route::currentRouteName(), ['dossier' => $dossier['id'], 'tab' => 'details   ']) }}"
-            class="px-4 py-2 {{ request('tab') === 'details' ? 'border-b-2 border-blue-500 text-blue-500' : 'text-gray-500' }}"
+            class="px-4 py-2 rounded-md capitalize transition-colors duration-100 text-button font-medium {{ request('tab') === 'details' ? 'bg-blue text-white' : 'hover:bg-blue hover:text-white focus:outline-none' }}"
         >
             Dossiergegevens
         </a>
     </div>
-
-    <div class="container">
-        @if (request('tab', 'overview') === 'overview')
-            @include('dossiers.partials.details.overview')
-        @elseif (request('tab') === 'documents')
-            @include('dossiers.partials.details.documents')
-        @elseif (request('tab') === 'details')
-            @include('dossiers.partials.details.details')
-        @endif
-    </div>
+    @if (request('tab', 'overview') === 'overview')
+        @include('dossiers.partials.details.overview')
+    @elseif (request('tab') === 'documents')
+        @include('dossiers.partials.details.documents')
+    @elseif (request('tab') === 'details')
+        @include('dossiers.partials.details.details')
+    @endif
 </x-layout>
