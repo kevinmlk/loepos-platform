9b9009d2a0438044a408b3fcde84eaa708e0d2fd
kevinmlk <r0900654@student.thomasmore.be>
2025-05-08 12:14:36 +0200

    Feat: Added debt model, controller and factory

---
+++ database/migrations/2025_05_08_100227_create_debts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Dossier;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('debts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Dossier::class);
+            $table->string('creditor');
+            $table->decimal('amount', 10, 5);
+            // $table->enum('status', )
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('debts');
+    }
+};
