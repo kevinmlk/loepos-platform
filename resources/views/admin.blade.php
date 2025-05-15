fe4f50bde44e4e74854989cfa9871fd297b57ba1
MiniJJackson <116001889+MiniJJackson@users.noreply.github.com>
2025-05-16 01:26:09 +0200

    Refactor: section added - updated styles

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -8,28 +8,32 @@
     </x-headerAdmin>
 
     {{-- Admin Tools --}}
-    <div class="flex flex-wrap gap-3">
-        @include('admin.partials.admin-card', [
-            'href' => '/admin/organisation',
-            'title' => 'Organisatie instellingen',
-            'description' => 'Wijzig de instellingen voor uw organisatie: naam, adres, …',
-            'icon' => 'bi bi-house-fill'
-        ])
+    <section class="border-2 border-light-gray rounded-xl p-6">
+        <h2>Administratie tools</h2>
 
-        @include('admin.partials.admin-card', [
-            'href' => '/admin/employees',
-            'title' => 'Medewerkers',
-            'description' => 'Toevoegen, bewerken en toewijzen van sociale medewerkers.',
-            'icon' => 'bi bi-person-square'
-        ])
+        <div class="flex flex-wrap gap-3 mt-4">
+            @include('admin.partials.admin-card', [
+                'href' => '/admin/organisation',
+                'title' => 'Organisatie instellingen',
+                'description' => 'Wijzig de instellingen voor uw organisatie: naam, adres, …',
+                'icon' => 'bi bi-house-fill'
+            ])
 
-        @include('admin.partials.admin-card', [
-            'href' => '/admin/clients',
-            'title' => 'Cliënten',
-            'description' => 'Toevoegen, bewerken en toewijzen van cliënten.',
-            'icon' => 'bi bi-people-fill'
-        ])
-    </div>
+            @include('admin.partials.admin-card', [
+                'href' => '/admin/employees',
+                'title' => 'Medewerkers',
+                'description' => 'Toevoegen, bewerken en toewijzen van sociale medewerkers.',
+                'icon' => 'bi bi-person-square'
+            ])
+
+            @include('admin.partials.admin-card', [
+                'href' => '/admin/clients',
+                'title' => 'Cliënten',
+                'description' => 'Toevoegen, bewerken en toewijzen van cliënten.',
+                'icon' => 'bi bi-people-fill'
+            ])
+        </div>
+    </section>
 
 
 </x-layout>
