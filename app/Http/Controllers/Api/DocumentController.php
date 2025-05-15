165ec8ea96e00f11bec27f949b6fe3877c52bdd9
kevinmlk <r0900654@student.thomasmore.be>
2025-05-15 12:35:48 +0200

    Feat(API): Added an extra security layer that checks if the sender is allowed to send requests

--- app/Http/Controllers/Api/DocumentController.php
+++ app/Http/Controllers/Api/DocumentController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Validator;
 
 use App\Services\DocumentService;
 use App\Models\Document;
+use App\Models\Dossier;
 
 class DocumentController extends Controller
 {
@@ -37,11 +38,18 @@ class DocumentController extends Controller
             'file' => 'required|file|mimes:pdf,png,jpg',
             'sender_email' => 'required|email',
             'receiver_email' => 'required|email',
-            // 'token' => 'required|string'
         ]);
 
         if ($validator->fails()) {
             return response()->json(['error' => $validator->errors()], 422);
+        }
+
+        // Store sender email from request
+        $senderEmail = $request->input('sender_email');
+
+        // Check in the db if the sender email matches a client or user
+        if (!$this->isSenderEmailAllowed($senderEmail)) {
+            return response()->json(['error' => 'Unauthorized sender email'], 403);
         }
 
         // Get file properties
@@ -74,9 +82,20 @@ class DocumentController extends Controller
     }
 
     /**
-    * TODO: Add check so see if the user/client is allowed to send email
-    * for uploading documents to the AI scanner.
+    * Check if the sender email is allowed.
+    *
+    * @param string $senderEmail
+    * @return bool
     */
+
+    private function isSenderEmailAllowed(string $senderEmail): bool
+    {
+        return Dossier::whereHas('client', function ($query) use ($senderEmail) {
+            $query->where('email', $senderEmail);
+        })->orWhereHas('user', function ($query) use ($senderEmail) {
+            $query->where('email', $senderEmail);
+        })->exists();
+    }
 
     /**
     * TODO: Change the email prefix of 'post@loepos.be' to for ex:
--- routes/api.php
+++ routes/api.php
@@ -6,10 +6,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\DocumentController;
 use App\Http\Middleware\VerifyApiToken;
 
-/**
-* TODO: Add an universal token to incoming request
-*/
-
 Route::middleware(VerifyApiToken::class)->group(function () {
     Route::post('/document', [DocumentController::class, 'store']);
 });
