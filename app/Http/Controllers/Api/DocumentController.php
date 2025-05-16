9abcc51bd069192345fe57c4c7f68a956ae695d7
kevinmlk <r0900654@student.thomasmore.be>
2025-05-16 14:33:55 +0200

    Feat(API): Documents send via the API are now also dynamically assigned to the right client dossier

--- app/Http/Controllers/Api/DocumentController.php
+++ app/Http/Controllers/Api/DocumentController.php
@@ -8,24 +8,19 @@ use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 
 use App\Services\DocumentService;
+use App\Services\DossierService;
+
 use App\Models\Document;
 use App\Models\Dossier;
 
 class DocumentController extends Controller
 {
-    protected $documentService;
+    protected $documentService, $dossierService;
 
-    public function __construct(DocumentService $documentService)
+    public function __construct(DocumentService $documentService, DossierService $dossierService)
     {
         $this->documentService = $documentService;
-    }
-
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
+        $this->dossierService = $dossierService;
     }
 
     /**
@@ -61,9 +56,15 @@ class DocumentController extends Controller
         $fullPath = Storage::disk('public')->path($filePath);
         $parsedData = $this->documentService->extractText($fullPath);
 
+        // Decode the parsed data to extract client information
+        $parsedDataArray = json_decode($parsedData, true);
+
+        // Determine the dossier_id using DocumentService
+        $dossierId = $this->dossierService->determineDossierId($parsedDataArray);
+
         // Create a new document record
         Document::create([
-            'dossier_id' => 1,
+            'dossier_id' => $dossierId ?? 1,
             'type' => Document::TYPE_INVOICE,
             'file_name' => $fileName,
             'file_path' => $filePath,
@@ -96,11 +97,5 @@ class DocumentController extends Controller
             $query->where('email', $senderEmail);
         })->exists();
     }
-
-    /**
-    * TODO: When a document is uploaded via the API, look to which client it belongs to
-    * and assigned it to that client.
-    */
-
 
 }
