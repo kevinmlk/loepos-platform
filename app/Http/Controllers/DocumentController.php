fbe601d1768d5669633add122888084ae1aa75e9
kevinmlk <r0900654@student.thomasmore.be>
2025-05-28 16:16:17 +0200

    Refactor: Removed getDocuments logic from controller function

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -34,30 +34,7 @@ class DocumentController extends Controller
     {
         $user = Auth::user();
 
-        if ($user->role === User::ROLE_ADMIN) {
-            // Admin: Get all documents belonging to the user's organization
-            $organizationId = $user->organization_id;
-
-            // Get all dossier IDs associated with the organization
-            $dossierIds = Dossier::whereHas('user', function ($query) use ($organizationId) {
-                $query->where('organization_id', $organizationId);
-            })->pluck('id');
-
-            // Get documents associated with the organization's dossiers
-            $documents = Document::whereIn('dossier_id', $dossierIds)->paginate(5);
-        } elseif ($user->role === User::ROLE_EMPLOYEE) {
-
-            // Employee: Get documents associated with the user's dossiers
-            $dossierIds = $user->dossiers()->pluck('id');
-
-            // Get documents associated with the user's dossiers
-            $documents = Document::whereIn('dossier_id', $dossierIds)->paginate(5);
-        } elseif ($user->role === User::ROLE_SUPERADMIN) {
-            $documents = Document::paginate(5);
-        } else {
-            // Default: No documents for other roles
-            $documents = collect(); // Empty collection
-        }
+        $documents = $this->documentService->getDocuments($user);
 
         return view('documents.index', [
             'documents' => $documents
