2f5947ccd7fd1a28e0258737bcc3598e922273ca
kevinmlk <r0900654@student.thomasmore.be>
2025-06-05 02:00:13 +0200

    Fix(API): Added route for uploading files for splitting

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Api\UploadController;
 use App\Http\Controllers\Api\DocumentController;
 use App\Http\Middleware\VerifyApiToken;
 
 Route::middleware(VerifyApiToken::class)->group(function () {
+    Route::post('/upload', [UploadController::class, 'store']);
     Route::post('/document', [DocumentController::class, 'store']);
 });
